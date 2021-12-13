88841ad85b4448f96e5156f74fb0ada72f746bda
Alexey Sheplyakov <asheplyakov@altlinux.org>
2021-12-13 16:00:57 +0400

    altlinux-0wn: actually can work in a chroot

--- altlinux-0wn.sh
+++ altlinux-0wn.sh
@@ -4,9 +4,12 @@ HOSTNAME='asheplyakov-rpi4b'
 SSH_PUB_KEY='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAEAQC4nppRTJ7Qa47L29r2H0lZHn6PaSTwribIYbC+rhn2Ar6toONVU2C+PBZCUAGzhep46Ojokl5fRGAPpA4Sj0MsOvu1o9qQP+EMuGDAuLQIF+c4YsGTVFUh/QrSXSwjyk2zL2PGiv9tS8MFLDqDNgR1VDv3MbCvlbnXQb8YGZRE7BQK9pkjCVwXCTobhOYPx6M8YO2NQRMKes6yZPrOwNj5cq5xfzA7bwYHjLlEPqXaMl1oFo8TEETmLta6/+j2FJo92xw66aOfRC+TMEyaQjj6M/1V5Q1wRvhGd54+O9dAyxNSpAtBdG48dTLs/gYn7F5XaGM6Yem7/LdMFm/kisayDFW2QGLrVWJ+Wv/RUaKWWteEwh+3ooRX2nC32I/1Cmzo+wPF6ERDw44jqJBlhXFoyb14y9n1YkxORbldbHJ7plQ3K/z1JEeAi6duj3zglMQvc03Dxd47GhMecJqUA1FBAVI2p4uaEx4vGQIor0GaeCEB+vP/w3E3HgOOgOgiCM8fB6XpHWUGMCGuAyamqqlAW4DAqRwzxzk7Q6a5CduYZaZZSpA+Bczog8HGIJ/seJH0HCh15pEB7lU6w/MlD9J4VJZl8v66x/cK+WQIjZS/THYPxuMXoB66I4hxz9usskkfWLq4siOokT86TalVBBi+LmGP5PocyA55nQD+ySF4g2xeRq78xeCd6sYCyQqvyifiirIiVQcCE804cHlyX997653JbYcYrsYShrSWfnbtsVzB+9VCkt6gAcqp604QnwF6N6BNTEg7ZQzALo//AHdDJq9iG40Af/TQWA/OjHcMk4fV5MhEurnkYxiFP4UHm8e/+oN5ILXB2udYokkW/A6dZY6sE/3H41IBbSYSIMLiI3FAEsJP3jii3/IELV7Ng4UgNXuFmpu4cLdFxLn/86VpC3rDmWYNKBVim3GG8IRaqqEHHM5eUh5xKpYr7SW17JbNrYRUL4mUnkO0p8bFgTpOsuVd2hfGgKmzC480E2ljwZqh3gcLCn0gtPbxcQ3h5+5HuRfAcEHV9rMa53q+h5qphqPXbbEeitf6zT2ntnn/RD/c5kM4iKJOYiw9+1CLh2kvtoLQ+I8jWJWxYmzz21LaVxNwkpi8moKPyVz+O5Q4GkDkmmtngJlTCci4P2EFEfPZJmjTAuj4OFyEDDNqHkADG16/6w7F9pooMGjQCpyUAzfG1VXFSOR+db1DwD41yHw4XbOy2c5mIkxZIFOma+OQBSQElAhfdz2DJe1ai7hfE//qRZKn1FiiSddOSwIDk3pmptjjMA/kZCxkl7KgXy9s7v5TUxO74gR9nHReIVTSUlyshgS1owmK00xoUiYH8RmKS4/aVxL8I/ODQhvw769X asheplyakov@asheplyakov-i5'
 HTTP_PROXY="http://10.42.0.1:8080/"
 
+IN_CHROOT=''
+
 if [ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/.)" ]; then
 	echo 'altlinux-setup: running in chroot' >&2
-	ACT_NOW=''
+	IN_CHROOT='yes'
+	ACT_NOW=' '
 fi
 
 copy_if_changed () {
@@ -117,7 +120,12 @@ ensure_systemd_resolved () {
 set_hostname () {
 	if test "`hostname`" != "$HOSTNAME"; then
 		echo "altlinux-setup:set_hostname: set-hostname $HOSTNAME" >&2
-		hostnamectl set-hostname "$HOSTNAME"
+		if [ "x$IN_CHROOT" = "x" ]; then
+			hostnamectl set-hostname "$HOSTNAME"
+		else
+			echo "altlinux-setup:set_hostname: running in chroot, will change /etc/hostname only" >&2
+			echo "$HOSTNAME" > /etc/hostname
+		fi
 	else
 		echo "altlinux-setup:set_hostname: NOP, hostname is already $HOSTNAME" >&2
 	fi
